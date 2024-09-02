83945d3099fdb6b87fa3f3d1fbb38999c14057ff
renanpires919 <renanpires919mx@gmail.com>
2024-09-02 10:31:06 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Evento de click no botao de burguer
+const navbar = document.querySelector('.navbar');
+const mobileNavbar = document.querySelector('.navbar__mobile');
+const button = document.querySelector('.burguer');
+
+button.addEventListener('click', function () {
+  mobileNavbar.classList.toggle('active');
+});
